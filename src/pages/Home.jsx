8456034cfbb8cbe54dda410303b611e1c6239af1
dloyolar <dieegolr@gmail.com>
2022-01-28 14:17:33 -0300

    feat: add Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import { Header } from '../components/Header';
 
 export const Home = () => {
-  return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
-  );
+  return <Header />;
 };

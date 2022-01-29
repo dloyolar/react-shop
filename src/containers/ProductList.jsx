8d3871320ad8d64dd20f750c8af434f2f17f487d
dloyolar <dieegolr@gmail.com>
2022-01-29 13:58:11 -0300

    feat: use new custom hook and pass props to children

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -2,22 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { ProductItem } from '../components/ProductItem';
 import '../styles/ProductList.scss';
 import axios from 'axios';
+import { useGetProducts } from '../hooks/useGetProducts';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(async () => {
-    const response = await axios(API);
-    setProducts(response.data);
-  }, []);
+  const { products } = useGetProducts(API);
 
   return (
     <section className="main-container">
       <div className="ProductList">
         {products.map((product) => (
-          <ProductItem />
+          <ProductItem product={product} key={product.id} />
         ))}
       </div>
     </section>

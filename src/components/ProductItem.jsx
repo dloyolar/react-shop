91edd050402b1fa7b2d4c63ec377e232cbec0d39
dloyolar <dieegolr@gmail.com>
2022-01-29 12:45:53 -0300

    wip: add state to handleCart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 
 import addToCart from '@icons/bt_add_to_cart.svg';
 
 export const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+
+  const handleCart = () => {
+    setCart([]);
+  };
+
   return (
     <div className="ProductItem">
       <img
@@ -15,7 +21,7 @@ export const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleCart} className="cart-icon">
           <img src={addToCart} alt="" />
         </figure>
       </div>

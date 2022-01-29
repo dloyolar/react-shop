d24ecb4636a8ed8722da7747ca23a8be6c0884e1
dloyolar <dieegolr@gmail.com>
2022-01-29 12:45:43 -0300

    feat: add toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import '@styles/Header.scss';
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import { Menu } from '@components/Menu';
 
 export const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -14,34 +22,37 @@ export const Header = () => {
         <img src={logo} alt="logo" className="nav-logo" />
         <ul>
           <li>
-            <a href="/">All</a>
+            <Link to="/">All</Link>
           </li>
           <li>
-            <a href="/">Clothes</a>
+            <Link to="/">Clothes</Link>
           </li>
           <li>
-            <a href="/">Electronics</a>
+            <Link to="/">Electronics</Link>
           </li>
           <li>
-            <a href="/">Furnitures</a>
+            <Link to="/">Furnitures</Link>
           </li>
           <li>
-            <a href="/">Toys</a>
+            <Link to="/">Toys</Link>
           </li>
           <li>
-            <a href="/">Others</a>
+            <Link to="/">Others</Link>
           </li>
         </ul>
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">email@example.com</li>
+          <li className="navbar-email" onClick={handleToggle}>
+            email@example.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
     </nav>
   );
 };
